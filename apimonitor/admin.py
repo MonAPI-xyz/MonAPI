cc650bfe8e64a2162bafd7bb0959c2140ceb431b
Andrew <susanto.andrew@ymail.com>
2022-09-20 19:29:33 +0700

    [GREEN] implement endpoint for view list API monitor

--- apimonitor/admin.py
+++ apimonitor/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from apimonitor.models import APIMonitor, APIMonitorBodyForm, APIMonitorRawBody, APIMonitorHeader, APIMonitorQueryParam, APIMonitorResult
+
+
+admin.site.register(APIMonitor)
+admin.site.register(APIMonitorBodyForm)
+admin.site.register(APIMonitorRawBody)
+admin.site.register(APIMonitorHeader)
+admin.site.register(APIMonitorQueryParam)
+admin.site.register(APIMonitorResult)
