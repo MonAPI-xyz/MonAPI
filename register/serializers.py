9b9ae5acd252273acd14c51ac3d4c596d27784e2
Lucky Susanto <luckyblock0@gmail.com>
2022-09-23 19:55:58 +0700

    [GREEN] Password policy is enforced

--- register/serializers.py
+++ register/serializers.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 
@@ -11,6 +13,13 @@ class RegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+    def validate_password(self, password):
+        try:
+            validate_password(password)
+        except ValidationError as exc:
+            raise serializers.ValidationError(str(exc))
+        return password
 
     def save(self):
         user = User(
