5c7758ad016fedd9f8731c5a8ac051b51d69eff3
Lucky Susanto <luckyblock0@gmail.com>
2022-09-25 01:03:10 +0700

    Review changes completed

--- password_validators/validators.py
+++ password_validators/validators.py
@@ -11,21 +11,11 @@ class NumberValidator(object):
                 code='password_contains_no_number',
             )
 
-    def get_help_text(self):
-        return _(
-            "Your password must contain at least 1 digit, 0-9."
-        )
-
 
 class UppercaseValidator(object):
     def validate(self, password, user=None):
         if not re.findall('[A-Z]', password):
             raise ValidationError(
                 _("The password must contain at least 1 uppercase letter, A-Z."),
-                  code='password_contains_no_capital',
+                code='password_contains_no_capital',
             )
-
-    def get_help_text(self):
-        return _(
-            "Your password must contain at least one capital"
-        )
--- register/views.py
+++ register/views.py
@@ -6,15 +6,14 @@ from register.serializers import RegistrationSerializer
 
 @api_view(['POST', ])
 def registration_view(request):
-    if request.method == 'POST':
-        serializer = RegistrationSerializer(data=request.data)
-        data = {}
-        if serializer.is_valid():
-            user = serializer.save()
-            data['response'] = 'Successfully registered new user.'
-            data['email'] = user.email
-            data['status_code'] = 201
-        else:
-            data = serializer.errors
-        return Response(data, status=status.HTTP_201_CREATED)
+    serializer = RegistrationSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        user = serializer.save()
+        data['response'] = 'Successfully registered new user.'
+        data['email'] = user.email
+        data['status_code'] = 201
+    else:
+        data = serializer.errors
+    return Response(data, status=status.HTTP_201_CREATED)
 
