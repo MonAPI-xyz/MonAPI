a83a75ad8238926c35ff2973817f807a7391664e
“HugoIr” <hugoirwanto23@gmail.com>
2022-09-27 16:39:29 +0700

    [REFACTOR] restructure serializer and views login

--- login/serializers.py
+++ login/serializers.py
@@ -1,17 +1,8 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-
 from rest_framework import serializers
 
-class LoginSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ['email', 'password',]
-        extra_kwargs = {
-            'email': {'required': True},
-            'password': {'write_only': True}
-        }
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
 
     def validate_attribute(self, attrs):
         email = attrs.get('email')
@@ -29,16 +20,3 @@ class LoginSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({'response': response}, code='authorization')
 
         
-
-    def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
-
-        user = authenticate(request=self.context.get('request'),
-                            username=email, email=email, password=password)
-        if not user:
-            response = 'Invalid email or password.'
-            raise serializers.ValidationError({'response':response}, code='authorization')
-        
-        attrs['user'] = user
-        return attrs
--- login/views.py
+++ login/views.py
@@ -1,8 +1,10 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.authtoken.models import Token
+
 from login.serializers import LoginSerializer
+from .utils import generate_token
 
 @api_view(["POST"])
 @authentication_classes([])
@@ -13,7 +15,13 @@ def user_login(request):
     
     serializer.validate_attribute(request.data)
     if serializer.is_valid():
-        user = serializer.validated_data['user']
+        user = authenticate(request=request,
+                            username=request.data['email'], 
+                            email=request.data['email'], 
+                            password=request.data['password'])
+        if not user:
+            data['response'] = 'Invalid email or password.'
+            return Response(data=data, status=status.HTTP_401_UNAUTHORIZED)   
 
         data['response'] = 'Sign-in successful.'
         data['email']= user.email
@@ -24,6 +32,3 @@ def user_login(request):
         data['response'] = 'Invalid email or password.'
         return Response(data=data, status=status.HTTP_401_UNAUTHORIZED)            
     
-def generate_token(user):
-    token, created = Token.objects.get_or_create(user=user)
-    return token.key
