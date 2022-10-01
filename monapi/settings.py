6e905c590aaf5fc18a81c17cfe58be2a42a7e559
Andrew <susanto.andrew@ymail.com>
2022-10-01 22:31:00 +0700

    [CHORES] fix missconfiguration for production environment

--- monapi/settings.py
+++ monapi/settings.py
@@ -87,6 +87,12 @@ REST_FRAMEWORK = {
 
 if os.getenv('PRODUCTION', '') == 'True':
     REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework.authentication.TokenAuthentication',
+        ],
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ],
         'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
         )
