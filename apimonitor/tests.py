7ff3df54d3298150ffcb539b0bba64d471cd519d
Lucky Susanto <luckyblock0@gmail.com>
2022-10-05 13:16:31 +0700

    [RED] Failed request shouldn't create any objects

--- apimonitor/tests.py
+++ apimonitor/tests.py
@@ -823,3 +823,164 @@ class ListAPIMonitor(APITestCase):
         response = self.client.post(create_new_monitor_test_path, data=received_json, format='json', **header)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(APIMonitor.objects.all().count(), 1)
+
+    def test_failed_attempt_because_query_params_doesnt_create_object(self):
+        # Create a user object
+        user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+        token = Token.objects.create(user=user)
+        header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+        monitor_value = {
+            'user': 'test@test.com',
+            'name': 'Test Monitor',
+            'method': 'GET',
+            'url': 'Test Path',
+            'schedule': '10MIN',
+            'body_type': 'FORM',
+        }
+
+        queryparam_value = [['key1', 'value1'], ['key2', 'value2']]
+        monitorheader_value = [['key3', 'value3']]
+        monitorbodyform_value = [['key4', 'value4'], ['key5', 'value5']]
+        monitorrawbody_value = "This doesn't matter since body_type is FORM"
+
+        # Expected JSON from frontend
+        received_json = {
+            'name': monitor_value['name'],
+            'method': monitor_value['method'],
+            'url': monitor_value['url'],
+            'schedule': monitor_value['schedule'],
+            'body_type': monitor_value['body_type'],
+            'query_params': "CORRUPTED_QUERY_PARAMS",
+            'headers': monitorheader_value,
+            'body_form': monitorbodyform_value,
+            'raw_body': monitorrawbody_value
+        }
+
+        # Get path
+        create_new_monitor_test_path = reverse('api-monitor-list')
+        response = self.client.post(create_new_monitor_test_path, data=received_json, format='json', **header)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(APIMonitor.objects.all().count(), 0)
+
+    def test_failed_attempt_because_headers_doesnt_create_object(self):
+        # Create a user object
+        user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+        token = Token.objects.create(user=user)
+        header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+        monitor_value = {
+            'user': 'test@test.com',
+            'name': 'Test Monitor',
+            'method': 'GET',
+            'url': 'Test Path',
+            'schedule': '10MIN',
+            'body_type': 'FORM',
+        }
+
+        queryparam_value = [['key1', 'value1'], ['key2', 'value2']]
+        monitorheader_value = [['key3', 'value3']]
+        monitorbodyform_value = [['key4', 'value4'], ['key5', 'value5']]
+        monitorrawbody_value = "This doesn't matter since body_type is FORM"
+
+        # Expected JSON from frontend
+        received_json = {
+            'name': monitor_value['name'],
+            'method': monitor_value['method'],
+            'url': monitor_value['url'],
+            'schedule': monitor_value['schedule'],
+            'body_type': monitor_value['body_type'],
+            'query_params': queryparam_value,
+            'headers': "CORRUPTED HEADER",
+            'body_form': monitorbodyform_value,
+            'raw_body': monitorrawbody_value
+        }
+
+        # Get path
+        create_new_monitor_test_path = reverse('api-monitor-list')
+        response = self.client.post(create_new_monitor_test_path, data=received_json, format='json', **header)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(APIMonitor.objects.all().count(), 0)
+        self.assertEqual(APIMonitorQueryParam.objects.all().count(), 0)
+
+    def test_failed_attempt_because_body_form_doesnt_create_object(self):
+        # Create a user object
+        user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+        token = Token.objects.create(user=user)
+        header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+        monitor_value = {
+            'user': 'test@test.com',
+            'name': 'Test Monitor',
+            'method': 'GET',
+            'url': 'Test Path',
+            'schedule': '10MIN',
+            'body_type': 'FORM',
+        }
+
+        queryparam_value = [['key1', 'value1'], ['key2', 'value2']]
+        monitorheader_value = [['key3', 'value3']]
+        monitorbodyform_value = [['key4', 'value4'], ['key5', 'value5']]
+        monitorrawbody_value = "This doesn't matter since body_type is FORM"
+
+        # Expected JSON from frontend
+        received_json = {
+            'name': monitor_value['name'],
+            'method': monitor_value['method'],
+            'url': monitor_value['url'],
+            'schedule': monitor_value['schedule'],
+            'body_type': monitor_value['body_type'],
+            'query_params': queryparam_value,
+            'headers': monitorheader_value,
+            'body_form': "CORRUPTED BODY FORM",
+            'raw_body': monitorrawbody_value
+        }
+
+        # Get path
+        create_new_monitor_test_path = reverse('api-monitor-list')
+        response = self.client.post(create_new_monitor_test_path, data=received_json, format='json', **header)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(APIMonitor.objects.all().count(), 0)
+        self.assertEqual(APIMonitorQueryParam.objects.all().count(), 0)
+        self.assertEqual(APIMonitorHeader.objects.all().count(), 0)
+
+    def test_failed_attempt_because_raw_body_doesnt_create_object(self):
+        # Create a user object
+        user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+        token = Token.objects.create(user=user)
+        header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+        monitor_value = {
+            'user': 'test@test.com',
+            'name': 'Test Monitor',
+            'method': 'GET',
+            'url': 'Test Path',
+            'schedule': '10MIN',
+            'body_type': 'RAW',
+        }
+
+        queryparam_value = [['key1', 'value1'], ['key2', 'value2']]
+        monitorheader_value = [['key3', 'value3']]
+        monitorbodyform_value = "This doesn't matter since body_type is RAW"
+        monitorrawbody_value = "RAW BODY VALUE"
+
+        # Expected JSON from frontend
+        received_json = {
+            'name': monitor_value['name'],
+            'method': monitor_value['method'],
+            'url': monitor_value['url'],
+            'schedule': monitor_value['schedule'],
+            'body_type': monitor_value['body_type'],
+            'query_params': queryparam_value,
+            'headers': monitorheader_value,
+            'body_form': monitorbodyform_value,
+            'raw_body': ["Corrupted", "Raw", "Body"]
+        }
+
+        # Get path
+        create_new_monitor_test_path = reverse('api-monitor-list')
+        response = self.client.post(create_new_monitor_test_path, data=received_json, format='json', **header)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(APIMonitor.objects.all().count(), 0)
+        self.assertEqual(APIMonitorQueryParam.objects.all().count(), 0)
+        self.assertEqual(APIMonitorHeader.objects.all().count(), 0)
