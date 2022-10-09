caad1f8c01f3d56d598d249247bd3dcc1823fde7
luthfifahlevi <77453015+luthfifahlevi@users.noreply.github.com>
2022-10-09 14:45:34 +0700

    feat: PBI-12-View Error Logs-Backend (#31)
    * [RED] initiate error logs app and create the tests
    
    * [GREEN] create implementation for view error logs and the detail
    
    * [REFACTOR] remove unused imports and fix styling
    
    Co-authored-by: Muhammad Fahlevi <muhammad.fahlevi@gdn-commerce.com>

---
+++ error_logs/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from apimonitor.models import APIMonitorResult
+from apimonitor.serializers import APIMonitorSerializer
+
+class ErrorLogsSerializer(serializers.ModelSerializer):
+  monitor = APIMonitorSerializer()
+  
+  class Meta:
+      model = APIMonitorResult
+      fields = [
+          'id',
+          'monitor',
+          'execution_time',
+          'date',
+          'hour',
+          'minute',
+          'response_time',
+          'success',
+          'log_response',
+          'log_error',
+      ]
