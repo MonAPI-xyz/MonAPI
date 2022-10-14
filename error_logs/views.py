8c330a87fb6e881ac47fea35bb0226e252fd0d6a
Muhammad Fahlevi <muhammad.fahlevi@gdn-commerce.com>
2022-10-14 17:26:30 +0700

    [GREEN] adding pagination to the implementation

--- error_logs/views.py
+++ error_logs/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from apimonitor.models import APIMonitorResult
 from error_logs.serializers import ErrorLogsSerializer
@@ -11,7 +13,14 @@ class ErrorLogsViewSet(mixins.ListModelMixin,
   queryset = APIMonitorResult.objects.all()
   serializer_class = ErrorLogsSerializer
   permission_classes = [IsAuthenticated]
-  
-  def get_queryset(self):
+
+  def retrieve(self, request, pk=None):
     queryset = APIMonitorResult.objects.filter(monitor__user=self.request.user, success=False)
+    obj = get_object_or_404(queryset, id=pk)
+    serializer = self.get_serializer(obj)
+    return Response(serializer.data)
+
+  def get_queryset(self):
+    limit = 1500
+    queryset = APIMonitorResult.objects.filter(monitor__user=self.request.user, success=False).order_by("-execution_time")[:limit:1]
     return queryset
--- monapi/settings.py
+++ monapi/settings.py
@@ -84,7 +84,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 if os.getenv('PRODUCTION', '') == 'True':
@@ -97,7 +99,9 @@ if os.getenv('PRODUCTION', '') == 'True':
         ],
         'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
-        )
+        ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
     }
 
 ROOT_URLCONF = 'monapi.urls'
