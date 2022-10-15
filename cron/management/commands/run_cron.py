9c745ec56ad215f9ded2bfcdb05ddb4adaecf374
Andrew <susanto.andrew@ymail.com>
2022-10-16 00:21:59 +0700

    [GREEN] improve cron delay accuracy

--- cron/management/commands/run_cron.py
+++ cron/management/commands/run_cron.py
@@ -32,85 +32,90 @@ class Command(BaseCommand):
     
     def worker(self):
         while True:
-            monitor_id = self.get_monitor_id_from_queue()
-            if monitor_id == None:
-                return
-            
-            print(f"[{timezone.now()}] Running cron for monitor id:{monitor_id}")
-            monitor = APIMonitor.objects.get(id=monitor_id)
-            
-            # Prepare headers
-            request_headers = {}
-            headers = APIMonitorHeader.objects.filter(monitor=monitor)
-            for header in headers:
-                request_headers[header.key] = header.value
-                
-            # Prepare request body
-            request_body = {}
-            if monitor.body_type == 'FORM':
-                forms = APIMonitorBodyForm.objects.filter(monitor=monitor)
-                for form in forms:
-                    request_body[form.key] = form.value
-            elif monitor.body_type == 'RAW':
-                try:
-                    raw_body = APIMonitorRawBody.objects.get(monitor=monitor)
-                    request_body = raw_body.body
-                except APIMonitorRawBody.DoesNotExist:
-                    pass
-            elif monitor.body_type == 'EMPTY':
-                request_body = None
-                
-            # Prepare query params
-            request_params = {}
-            query_params = APIMonitorQueryParam.objects.filter(monitor=monitor)
-            for param in query_params:
-                request_params[param.key] = param.value
-                    
-            resp = None
-            exception = ''
-            
-            start = time.perf_counter()
+            # Prevent halt worker
             try:
-                if monitor.method == 'GET':
-                    resp = requests.get(monitor.url, params=request_params, headers=request_headers, timeout=30)
-                elif monitor.method == 'POST':
-                    resp = requests.post(monitor.url, params=request_params, data=request_body, headers=request_headers, timeout=30)
-                elif monitor.method == 'PATCH':
-                    resp = requests.patch(monitor.url, params=request_params, data=request_body, headers=request_headers, timeout=30)
-                elif monitor.method == 'PUT':
-                    resp = requests.put(monitor.url, params=request_params, data=request_body, headers=request_params, timeout=30)
-                elif monitor.method == 'DELETE':
-                    resp = requests.delete(monitor.url, params=request_params, data=request_body, headers=request_headers, timeout=30)
-            except Exception as e:
-                exception = str(e)
-            request_time = (time.perf_counter() - start) * 1000 # Convert from s to ms
-            
-            success = False
-            status_code = -1
-            resp_content = ''
-            if resp is not None:
-                if resp.status_code >= 200 and resp.status_code <= 299:
-                    success = True
-                resp_content = resp.content.decode('utf-8', errors='ignore')
-                status_code = resp.status_code
+                monitor_id = self.get_monitor_id_from_queue()
+                if monitor_id == None:
+                    return
                 
-            execution_time = timezone.localtime()
-            APIMonitorResult.objects.create(
-                monitor=monitor,
-                execution_time=execution_time,
-                response_time=request_time,
-                success=success,
-                status_code=status_code,
-                log_response=resp_content,
-                log_error=exception,
-            )
-            print(f"[{timezone.now()}] Done run cron for monitor id:{monitor_id}")
-            self.q.task_done()
+                execution_time = timezone.localtime()
+                print(f"[{timezone.now()}] Running cron for monitor id:{monitor_id}")
+                monitor = APIMonitor.objects.get(id=monitor_id)
+                
+                # Prepare headers
+                request_headers = {}
+                headers = APIMonitorHeader.objects.filter(monitor=monitor)
+                for header in headers:
+                    request_headers[header.key] = header.value
+                    
+                # Prepare request body
+                request_body = {}
+                if monitor.body_type == 'FORM':
+                    forms = APIMonitorBodyForm.objects.filter(monitor=monitor)
+                    for form in forms:
+                        request_body[form.key] = form.value
+                elif monitor.body_type == 'RAW':
+                    try:
+                        raw_body = APIMonitorRawBody.objects.get(monitor=monitor)
+                        request_body = raw_body.body
+                    except APIMonitorRawBody.DoesNotExist:
+                        pass
+                elif monitor.body_type == 'EMPTY':
+                    request_body = None
+                    
+                # Prepare query params
+                request_params = {}
+                query_params = APIMonitorQueryParam.objects.filter(monitor=monitor)
+                for param in query_params:
+                    request_params[param.key] = param.value
+                        
+                resp = None
+                exception = ''
+                
+                start = time.perf_counter()
+                try:
+                    if monitor.method == 'GET':
+                        resp = requests.get(monitor.url, params=request_params, headers=request_headers, timeout=30)
+                    elif monitor.method == 'POST':
+                        resp = requests.post(monitor.url, params=request_params, data=request_body, headers=request_headers, timeout=30)
+                    elif monitor.method == 'PATCH':
+                        resp = requests.patch(monitor.url, params=request_params, data=request_body, headers=request_headers, timeout=30)
+                    elif monitor.method == 'PUT':
+                        resp = requests.put(monitor.url, params=request_params, data=request_body, headers=request_params, timeout=30)
+                    elif monitor.method == 'DELETE':
+                        resp = requests.delete(monitor.url, params=request_params, data=request_body, headers=request_headers, timeout=30)
+                except Exception as e:
+                    exception = str(e)
+                request_time = (time.perf_counter() - start) * 1000 # Convert from s to ms
+                
+                success = False
+                status_code = -1
+                resp_content = ''
+                if resp is not None:
+                    if resp.status_code >= 200 and resp.status_code <= 299:
+                        success = True
+                    resp_content = resp.content.decode('utf-8', errors='ignore')
+                    status_code = resp.status_code
+                    
+                APIMonitorResult.objects.create(
+                    monitor=monitor,
+                    execution_time=execution_time,
+                    response_time=request_time,
+                    success=success,
+                    status_code=status_code,
+                    log_response=resp_content,
+                    log_error=exception,
+                )
+                print(f"[{timezone.now()}] Done run cron for monitor id:{monitor_id}")
+                self.q.task_done()
+            except:
+                pass
 
     def handle(self, *args, **kwargs):
         # Run consumer worker
         self.stop_signal.clear()
         thread_pool = []
+        last_triggered = {}
         
         consumer_count = 3
         try:
@@ -135,25 +140,31 @@ class Command(BaseCommand):
         }
         
         try:
+            # Fill last_triggered
+            api_monitors = APIMonitor.objects.all()
+            for monitor in api_monitors:
+                exists = APIMonitorResult.objects.filter(monitor=monitor).exists()
+                if exists:
+                    last_result = APIMonitorResult.objects.filter(monitor=monitor).last()
+                    last_triggered[monitor.id] = last_result.execution_time
+                
+            # Cron loop function
             while True:
+                last_run = timezone.now()
                 api_monitors = APIMonitor.objects.all()
                 for monitor in api_monitors:
-                    # Minus 1 second adjustment for python process
-                    result_filter_time = timezone.now() - timedelta(seconds=schedule_in_seconds[monitor.schedule]-1)
-                    
-                    last_result = APIMonitorResult.objects.filter(
-                        monitor=monitor, 
-                        execution_time__gt=result_filter_time
-                    ).exists()
-                    
-                    if last_result:
+                    result_filter_time = last_run - timedelta(seconds=schedule_in_seconds[monitor.schedule])
+                    if monitor.id in last_triggered and result_filter_time < last_triggered[monitor.id]:
                         continue
                     
+                    last_triggered[monitor.id] = last_run
                     self.q.put(monitor.id)
                     
                 # Add delay before next check
                 mock_cron_interrupt()
-                time.sleep(1)  
+                next_run = last_run + timedelta(seconds=60) 
+                sleep_duration = (next_run - timezone.now()).total_seconds()
+                time.sleep(max(sleep_duration, 0))  
         except BaseException as e:
             # Gracefully shutdown thread worker
             self.q.join()
