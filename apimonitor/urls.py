035e4a3a262bbb9f1db09d1f897796d8a4fa9604
Andrew <susanto.andrew@ymail.com>
2022-10-18 20:53:56 +0700

    [RED] add tests for alerts configuration

--- apimonitor/urls.py
+++ apimonitor/urls.py
@@ -3,9 +3,9 @@ from rest_framework import routers
 
 from apimonitor import views
 
-
 router = routers.DefaultRouter()
 router.register(r'', views.APIMonitorViewSet, basename='api-monitor')
+
 urlpatterns = [
     path('', include(router.urls)),
 ]
