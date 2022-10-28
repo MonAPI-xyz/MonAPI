f74f2cb9bb0dedca122aad926951aed3ebd5b845
Andrew <susanto.andrew@ymail.com>
2022-10-28 19:20:16 +0700

    feat: PBI-18 alerts configuration (#43)
    * [RED] add tests for alerts configuration
    
    * [GREEN] implement alerts configuration

---
+++ alerts/views.py
@@ -0,0 +1,26 @@
+from rest_framework import views, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from alerts.serializers import AlertsConfigurationSerializer
+from apimonitor.models import AlertsConfiguration
+
+class AlertConfiguration(views.APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, format=None):
+        config, _ = AlertsConfiguration.objects.get_or_create(user=request.user)
+        serializer = AlertsConfigurationSerializer(config)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        config, _ = AlertsConfiguration.objects.get_or_create(user=request.user)
+        serializer = AlertsConfigurationSerializer(config, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
+    
--- apimonitor/admin.py
+++ apimonitor/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apimonitor.models import APIMonitor, APIMonitorBodyForm, APIMonitorRawBody, APIMonitorHeader, APIMonitorQueryParam, APIMonitorResult, AssertionExcludeKey
+from apimonitor.models import APIMonitor, APIMonitorBodyForm, APIMonitorRawBody, APIMonitorHeader, APIMonitorQueryParam, APIMonitorResult, AssertionExcludeKey, AlertsConfiguration
 
 
 admin.site.register(APIMonitor)
@@ -10,3 +10,4 @@ admin.site.register(APIMonitorHeader)
 admin.site.register(APIMonitorQueryParam)
 admin.site.register(APIMonitorResult)
 admin.site.register(AssertionExcludeKey)
+admin.site.register(AlertsConfiguration)
