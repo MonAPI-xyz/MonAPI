6d9c9c0a5b783ec27b347f4326d69892e253ff2f
Andrew <susanto.andrew@ymail.com>
2022-11-03 13:13:36 +0700

    feat: PBI 22 add forget password (#69)
    * [RED] add test for forget password
    
    * [GREEN] impelemnt forget password endpoint
    
    * [REFACTOR] removes bug and code smell
    
    * [REFACTOR] update from email config
    
    * [REFACTOR] change forget password email styling

---
+++ forget_password/models.py
@@ -0,0 +1,8 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import User
+
+class ForgetPasswordToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    key = models.CharField(max_length=256, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
