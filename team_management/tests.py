c2b979166cac9906456f41688a9426eba3b9bb96
“HugoIr” <hugoirwanto23@gmail.com>
2022-11-20 12:22:37 +0700

    [RED] create tests for edit team profile

--- team_management/tests.py
+++ team_management/tests.py
@@ -1,8 +1,10 @@
+import os
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from login.models import Team, TeamMember, MonAPIToken
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class TeamManagementTests(APITestCase):
   url = reverse('team-management-list')
@@ -71,4 +73,87 @@ class TeamManagementTests(APITestCase):
     response = self.client.post(self.url, data=new_team_request, format='json', **header)
     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
     self.assertEqual(response.data['name'], "test team")
+
+  def test_when_authenticated_and_update_with_empty_team_name_then_return_bad_request(self):
+    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+    team = Team.objects.create(name='myteam')
+    team_member = TeamMember.objects.create(team=team, user=user)
+    
+    token = MonAPIToken.objects.create(team_member=team_member)
+    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+    new_team_request = {
+      "name": "",
+    }
+    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
+    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
+    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
+    self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    self.assertEqual(response.data, {
+        "error": "Please make sure your [team name] is exist!"
+    })  
+
+  def test_when_authenticated_and_update_with_not_empty_team_name_then_return_success(self):
+    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+    team = Team.objects.create(name='myteam')
+    team_member = TeamMember.objects.create(team=team, user=user)
+    
+    token = MonAPIToken.objects.create(team_member=team_member)
+    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+    new_team_request = {
+      "name": "test team",
+    }
+    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
+    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
+    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data['name'], "test team")
+
+  def test_when_authenticated_and_update_with_complete_team_data_then_return_success(self):
+    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+    team = Team.objects.create(name='myteam')
+    team_member = TeamMember.objects.create(team=team, user=user)
+    
+    token = MonAPIToken.objects.create(team_member=team_member)
+    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+    new_team_request = {
+      "name": "test team",
+      "description": "test description",
+      "logo": "testLogo.png",
+    }    
+    
+    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
+    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
+    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data['name'], "test team")
+
   
+  def test_when_authenticated_and_update_with_new_image_then_return_success(self):
+    
+    image = SimpleUploadedFile(name='test_image.jpg', content=b'', content_type='image/jpeg')
+    new_image = SimpleUploadedFile(name='test_new_image.jpg', content=b'', content_type='image/jpeg')
+
+    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+    team = Team.objects.create(name='myteam', description='123', logo=image)
+    team_logo_path = team.logo.path
+
+    team_member = TeamMember.objects.create(team=team, user=user)
+    
+    token = MonAPIToken.objects.create(team_member=team_member)
+    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+    new_team_request = {
+      "name": "test team",
+      "description": "test description",
+      "logo": new_image,
+    }    
+    
+    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
+    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
+    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data['name'], "test team")
+    self.assertEqual(os.path.exists(team_logo_path), False )
