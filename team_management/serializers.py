3476de58650ef7e54153651afd17c010a82d437e
Ferdi Fadillah <46517394+ferdifdi@users.noreply.github.com>
2022-11-20 19:44:07 +0700

    feat: Pbi 25 view current team profile backend (#94)
    * [RED] test view current team profile
    
    * [GREEN] implement view current team profile
    
    * [RED] test new team management serializers
    
    * [GREEN] implement new team management serializers
    
    * [RED] test fix serializers
    
    * [GREEN] fix serializers

---
+++ team_management/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from login.models import Team, TeamMember
+from django.contrib.auth.models import User
+
+
+class TeamUserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username', 
+            'email', 
+            'first_name', 
+            'last_name'
+        ]
+
+class TeamMemberSerializers(serializers.ModelSerializer):
+    user = TeamUserSerializers()
+    class Meta:        
+        model = TeamMember
+        fields = [
+            'team', 
+            'user'
+        ]
+
+class TeamManagementSerializers(serializers.ModelSerializer):
+    teammember = TeamMemberSerializers(many=True)
+    class Meta:
+        model = Team
+        fields = [
+            'id',
+            'name',
+            'logo',
+            'description',
+            'teammember'
+        ]
+
+
+
+
