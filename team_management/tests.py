af5e0b75b3e6545e948ef4e36959ce690f2f192c
“HugoIr” <hugoirwanto23@gmail.com>
2022-11-20 13:03:24 +0700

    [RED] add and renew tests edit team profile

--- team_management/tests.py
+++ team_management/tests.py
@@ -74,43 +74,7 @@ class TeamManagementTests(APITestCase):
     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
     self.assertEqual(response.data['name'], "test team")
 
-  def test_when_authenticated_and_update_with_empty_team_name_then_return_bad_request(self):
-    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
-    team = Team.objects.create(name='myteam')
-    team_member = TeamMember.objects.create(team=team, user=user)
-    
-    token = MonAPIToken.objects.create(team_member=team_member)
-    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
-
-    new_team_request = {
-      "name": "",
-    }
-    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
-    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
-    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
-    self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    self.assertEqual(response.data, {
-        "error": "Please make sure your [team name] is exist!"
-    })  
-
-  def test_when_authenticated_and_update_with_not_empty_team_name_then_return_success(self):
-    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
-    team = Team.objects.create(name='myteam')
-    team_member = TeamMember.objects.create(team=team, user=user)
-    
-    token = MonAPIToken.objects.create(team_member=team_member)
-    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
-
-    new_team_request = {
-      "name": "test team",
-    }
-    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
-    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
-    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(response.data['name'], "test team")
-
-  def test_when_authenticated_and_update_with_complete_team_data_then_return_success(self):
+  def test_when_authenticated_and_update_with_not_empty_team_name_then_return_bad_request(self):
     user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
     team = Team.objects.create(name='myteam')
     team_member = TeamMember.objects.create(team=team, user=user)
@@ -122,13 +86,33 @@ class TeamManagementTests(APITestCase):
       "name": "test team",
       "description": "test description",
       "logo": "testLogo.png",
+    }
+    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
+    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
+    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
+    self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    self.assertEqual(response.data, {
+        "error": "Team name cannot be changed!"
+    })
+
+  def test_when_authenticated_and_update_with_required_team_data_then_return_success(self):
+    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+    team = Team.objects.create(name='myteam')
+    team_member = TeamMember.objects.create(team=team, user=user)
+    
+    token = MonAPIToken.objects.create(team_member=team_member)
+    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+    new_team_request = {
+      "description": "test description",
+      "logo": "testLogo.png",
     }    
     
     target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
     edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
     response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(response.data['name'], "test team")
+    self.assertEqual(response.data['name'], "myteam")
 
   
   def test_when_authenticated_and_update_with_new_image_then_return_success(self):
@@ -146,7 +130,6 @@ class TeamManagementTests(APITestCase):
     header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
 
     new_team_request = {
-      "name": "test team",
       "description": "test description",
       "logo": new_image,
     }    
@@ -155,5 +138,29 @@ class TeamManagementTests(APITestCase):
     edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
     response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(response.data['name'], "test team")
+    self.assertEqual(response.data['name'], "myteam")
     self.assertEqual(os.path.exists(team_logo_path), False )
+
+  def test_when_authenticated_and_update_only_description_then_return_success(self):
+    
+    image = SimpleUploadedFile(name='test_image.jpg', content=b'', content_type='image/jpeg')
+
+    user = User.objects.create_user(username="test@test.com", email="test@test.com", password="Test1234")
+    team = Team.objects.create(name='myteam', description='123', logo=image)
+    team_logo_path = team.logo.path
+
+    team_member = TeamMember.objects.create(team=team, user=user)
+    
+    token = MonAPIToken.objects.create(team_member=team_member)
+    header = {'HTTP_AUTHORIZATION': f"Token {token.key}"}
+
+    new_team_request = {
+      "description": "test description",
+    }    
+    
+    target_team_id = TeamMember.objects.filter(team=team)[:1].get().id
+    edit_team_url = reverse('team-management-detail', kwargs={'pk': target_team_id})    
+    response = self.client.put(edit_team_url, data=new_team_request, format='json', **header)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data['name'], "myteam")
+    self.assertEqual(os.path.exists(team_logo_path), True )
