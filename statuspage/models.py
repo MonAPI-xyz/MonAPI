4aa31144d5fa451af024a618dc99d8fca7e857f0
Andrew <susanto.andrew@ymail.com>
2022-11-29 21:09:31 +0700

    [REFACTOR] add unique constraint on path

--- statuspage/models.py
+++ statuspage/models.py
@@ -3,7 +3,7 @@ from login.models import Team
 
 class StatusPageConfiguration(models.Model):
     team = models.OneToOneField(Team, on_delete=models.CASCADE)
-    path = models.CharField(max_length=64, blank=True)
+    path = models.CharField(max_length=64, blank=True, unique=True)
     
 class StatusPageCategory(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
