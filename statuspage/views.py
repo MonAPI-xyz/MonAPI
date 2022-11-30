0fc8561ea72c7aee276f9473dd1f008e8acdfde9
Muhammad Fahlevi <muhammad.fahlevi@gdn-commerce.com>
2022-11-30 18:36:24 +0700

    [GREEN] create implementation for status page dashboard

--- statuspage/views.py
+++ statuspage/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from datetime import timedelta
+from django.db.models import Count, Q
+from django.utils import timezone
 from rest_framework import views, status, viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from apimonitor.models import APIMonitorResult
 from statuspage.models import StatusPageConfiguration, StatusPageCategory
-from statuspage.serializers import StatusPageConfgurationSerializers, StatusPageCategorySerializers
+from statuspage.serializers import StatusPageConfgurationSerializers, StatusPageCategorySerializers, StatusPageDashboardSerializers
 
 class StatusPageConfigurationView(views.APIView):
     permission_classes = [IsAuthenticated]
@@ -42,3 +45,54 @@ class StatusPageCategoryViewSet(mixins.ListModelMixin,
         request.data['team'] = request.auth.team.pk
         return super().create(request, *args, **kwargs)
     
+
+class StatusPageDashboardViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    
+    queryset = StatusPageCategory.objects.all()
+    serializer_class = StatusPageDashboardSerializers
+    permission_classes = []
+    pagination_class = None
+
+    def list(self, request):
+        statusPageConfig = StatusPageConfiguration.objects.filter(path=self.request.GET.get('path'))
+        if len(statusPageConfig) == 0:
+            return Response(data={"error": "Please make sure your URL path is exist!"}, status=status.HTTP_404_NOT_FOUND)
+        
+        queryset = StatusPageCategory.objects.filter(team=statusPageConfig[0].team)
+
+        for category in queryset:
+            success_rate_category = []
+            ApiMonitorCategory = APIMonitorResult.objects \
+                .filter(monitor__team=statusPageConfig[0].team, monitor__status_page_category=category)
+
+            if len(ApiMonitorCategory) == 0:
+                category.success_rate_category = success_rate_category
+                continue
+            
+            #success_rate per category
+            last_24_hour = timezone.now() - timedelta(hours=24)
+            for _ in range(24):
+                start_time = last_24_hour
+                end_time = last_24_hour + timedelta(hours=1)
+                
+                # Average success rate
+                success_count = ApiMonitorCategory.filter(execution_time__gte=start_time, execution_time__lte=end_time) \
+                    .aggregate(
+                        s=Count('success', filter=Q(success=True)), 
+                        f=Count('success', filter=Q(success=False)),
+                        total=Count('pk'),
+                    )
+
+                success_rate_category.append({
+                    "start_time": start_time,
+                    "end_time" : end_time,
+                    "success": success_count['s'],
+                    "failed" : success_count['f']
+                })
+
+                last_24_hour += timedelta(hours=1)
+
+            category.success_rate_category = success_rate_category
+
+        serializer = StatusPageDashboardSerializers(queryset, many=True)
+        return Response(serializer.data)
