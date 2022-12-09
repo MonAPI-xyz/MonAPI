4095e709d7cc450d493c2e79ab22925a2189476d
Andrew <susanto.andrew@ymail.com>
2022-12-09 11:56:32 +0700

    [FIX] fix status page config models unique blank

--- statuspage/models.py
+++ statuspage/models.py
@@ -3,7 +3,7 @@ from login.models import Team
 
 class StatusPageConfiguration(models.Model):
     team = models.OneToOneField(Team, on_delete=models.CASCADE)
-    path = models.CharField(max_length=64, blank=True, unique=True)
+    path = models.CharField(max_length=64, null=True, blank=False, unique=True, default=None)
     
 class StatusPageCategory(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
