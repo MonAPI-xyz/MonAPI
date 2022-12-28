5add97d1b11b8661735324454a4cfc8fed1ce862
Andrew <susanto.andrew@ymail.com>
2022-12-28 12:01:50 +0700

    feat: change status page dashboard query and add forget password validation (#114)
    * [REFACTOR] update query for status page dashboard
    
    * [FIX] fix change password validation
    
    * [CHORES] add technical documentation and youtube link

--- forget_password/serializers.py
+++ forget_password/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 
 class RequestForgetPasswordTokenSerializer(serializers.Serializer):
@@ -12,3 +14,10 @@ class ForgetPasswordTokenCheckSerializer(serializers.Serializer):
 class ResetPasswordSerializer(serializers.Serializer):
     password = serializers.CharField(max_length=128)
     key = serializers.CharField(max_length=256)
+    
+    def validate_password(self, password):
+        try:
+            validate_password(password)
+        except ValidationError as exc:
+            raise exc
+        return password
